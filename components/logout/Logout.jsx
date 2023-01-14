d9c0ed50d421317d2030c528004a1f3fade7db6c
bisht-xp <bishtkamal2907@gmail.com>
2023-01-14 18:56:20 +0530

    create logout components

---
+++ components/logout/Logout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+
+export default function Logout() {
+  const { logout } = useAuth();
+  return (
+    <button
+      onClick={logout}
+      type="button"
+      className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+    >
+      logout
+    </button>
+  );
+}
