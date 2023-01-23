2db3143deacb02d467be8856013215e649f6b446
bisht-xp <bishtkamal2907@gmail.com>
2023-01-23 14:42:16 +0530

    finish with frontend

--- pages/messages.jsx
+++ pages/messages.jsx
@@ -11,8 +11,52 @@ import Topbar from "../components/topbar/Topbar";
 import Conversation from "../components/conversations/Conversation";
 import Message from "../components/message/Message";
 import Chat from "../components/chat/Chat";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useRef, useState } from "react";
 
-export default function messages() {
+export default function messages({ conversationData }) {
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const scrollRef = useRef();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get(`/api/messages/${currentChat?._id}`);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+
+  const clickHandler = async (event) => {
+    event.preventDefault();
+    if (newMessage.trim().length !== 0) {
+      const message = {
+        sender: auth.user._id,
+        text: newMessage,
+        conversationId: currentChat._id,
+      };
+
+      try {
+        const res = await axios.post("/api/messages", message);
+        setMessages([...messages, res.data]);
+        setNewMessage("");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <>
       <Head>
@@ -28,70 +72,88 @@ export default function messages() {
                 <p className="text-md font-bold hidden md:block">Messenger</p>
               </div>
               <div className="contacts p-2 flex-1 scrollbar overflow-y-scroll">
-                <Conversation />
-                <Conversation />
-                <Conversation />
-                <Conversation />
+                {conversationData.map((conv) => (
+                  <div onClick={() => setCurrentChat(conv)} key={conv._id}>
+                    <Conversation conversation={conv} currentUser={auth.user} />
+                  </div>
+                ))}
               </div>
             </section>
             <section className="flex flex-col flex-auto border-l">
-              <Message />
-              <div className="chat-body p-4 flex-1 scrollbar overflow-y-scroll">
-                <Chat />
-                <Chat own={true}/>
-                <Chat />
-                <Chat own={true}/>
-                <Chat />
-              </div>
-              <div className="chat-footer flex-none">
-                <div className="flex flex-row items-center p-4">
-                  <button
-                    type="button"
-                    className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
-                  >
-                    <AddCircle />
-                  </button>
-                  <button
-                    type="button"
-                    className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
-                  >
-                    <Photo />
-                  </button>
-                  <button
-                    type="button"
-                    className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
-                  >
-                    <PhotoCamera />
-                  </button>
-                  <button
-                    type="button"
-                    className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
-                  >
-                    <Mic />
-                  </button>
-                  <div className="relative flex-grow">
-                    <label>
-                      <input
-                        className="rounded-full py-2 pl-3 pr-10 w-full border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none text-gray-600 focus:shadow-md transition duration-300 ease-in"
-                        type="text"
-                        placeholder="Aa"
-                      />
+              {currentChat ? (
+                <>
+                  <Message currentChat={currentChat} />
+                  <div className="chat-body p-4 flex-1 scrollbar overflow-y-scroll">
+                    {messages.map((message) => (
+                      <div key={message._id} ref={scrollRef}>
+                        <Chat
+                          message={message}
+                          own={message.sender === auth.user._id}
+                        />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="chat-footer flex-none">
+                    <div className="flex flex-row items-center p-4">
                       <button
                         type="button"
-                        className="absolute top-0 right-0 mt-2 mr-3 flex flex-shrink-0 focus:outline-none text-blue-600 hover:text-blue-700 w-6 h-6"
+                        className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
                       >
-                        <Send />
+                        <AddCircle />
                       </button>
-                    </label>
+                      <button
+                        type="button"
+                        className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
+                      >
+                        <Photo />
+                      </button>
+                      <button
+                        type="button"
+                        className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
+                      >
+                        <PhotoCamera />
+                      </button>
+                      <button
+                        type="button"
+                        className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
+                      >
+                        <Mic />
+                      </button>
+                      <div className="relative flex-grow">
+                        <label>
+                          <input
+                            className="rounded-full py-2 pl-3 pr-10 w-full border border-gray-200 bg-gray-200 focus:bg-white focus:outline-none text-gray-600 focus:shadow-md transition duration-300 ease-in"
+                            type="text"
+                            placeholder="Aa"
+                            vlaue={newMessage}
+                            onChange={(event) =>
+                              setNewMessage(event.target.value)
+                            }
+                          />
+                          <button
+                            type="submit"
+                            className="absolute top-0 right-0 mt-2 mr-3 flex flex-shrink-0 focus:outline-none text-blue-600 hover:text-blue-700 w-6 h-6"
+                            onClick={clickHandler}
+                          >
+                            <Send />
+                          </button>
+                        </label>
+                      </div>
+                      <button
+                        type="button"
+                        className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
+                      >
+                        <ThumbUp />
+                      </button>
+                    </div>
                   </div>
-                  <button
-                    type="button"
-                    className="flex flex-shrink-0 focus:outline-none mx-2 text-blue-600 hover:text-blue-700 w-6 h-6"
-                  >
-                    <ThumbUp />
-                  </button>
-                </div>
-              </div>
+                </>
+              ) : (
+                <span className="  text-5xl text-gray-200 p-10 cursor-default flex-1">
+                  Open a conversation to start a chat.
+                </span>
+              )}
             </section>
           </main>
         </div>
@@ -99,3 +161,13 @@ export default function messages() {
     </>
   );
 }
+
+export const getServerSideProps = async ({ req, res }) => {
+  const getConversation = await axios.get(
+    `http://localhost:3000/api/conversations/${req.user._id}`
+  );
+  const conversationData = getConversation.data;
+  return {
+    props: { conversationData },
+  };
+};
