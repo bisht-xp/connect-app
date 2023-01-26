4920d2b20e3db57be7846fa647085465a16d14be
bisht-xp <bishtkamal2907@gmail.com>
2023-01-26 19:20:52 +0530

    update feed components adding story compoents

--- components/feed/Feed.jsx
+++ components/feed/Feed.jsx
@@ -1,18 +1,26 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
+import Story from "../story/Story";
 // import { Posts } from "../../dummyData";
 
-export default function Feed({posts}) {
+export default function Feed({ posts }) {
   return (
-    <div className="w-full md:ml-64 lg:basis-2/3 ">
-    
-      <div className="p-2 md:p-5">
-        <Share />
-        {posts.map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
-        {/* <Post /> */}
-      </div>
-    </div>
+    // <div className="w-full md:ml-64 lg:basis-2/3 ">
+
+    //   <div className="p-2 md:p-5">
+    //     <Share />
+    //     {posts.map((post) => (
+    //       <Post key={post._id} post={post} />
+    //     ))}
+    //     {/* <Post /> */}
+    //   </div>
+    // </div>
+    <>
+      <Story />
+      <Share />
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </>
   );
 }
