4103664a6b46e95d7458d7d4818025b130994101
bisht-xp <bishtkamal2907@gmail.com>
2023-01-28 17:56:45 +0530

    update route

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -38,7 +38,7 @@ router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
     });
     res.status(200).json(conversation)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json("Not Found");
   }
 });
 
